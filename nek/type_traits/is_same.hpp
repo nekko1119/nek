509a339d258b706b6857738fd0ec72e136bd394d
nekko1119 <taku5050@mail.goo.ne.jp>
2013-04-20 03:13:47 +0900

    add is_same.hpp

---
+++ nek/type_traits/is_same.hpp
@@ -0,0 +1,22 @@
+﻿#ifndef NEK_TYPE_TRAITS_IS_SAME_HPP
+#define NEK_TYPE_TRAITS_IS_SAME_HPP
+
+#include <nek/type_traits/integral_constant.hpp>
+
+namespace nek
+{
+	template <class T, class U>
+	struct is_same
+		: public false_type
+	{
+	};
+
+	template <class T>
+	struct is_same<T, T>
+		: public true_type
+	{
+	};
+
+}
+
+#endif
