eaa25b40a36533480683b981c64058ba5ea87d46
nekko1119 <taku5050@mail.goo.ne.jp>
2013-04-19 17:12:22 +0900

    Utilityを追加

---
+++ nektest/utility_test.cpp
@@ -0,0 +1,18 @@
+﻿#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace nektest
+{		
+	TEST_CLASS(utility_test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: テスト コードをここに挿入します
+		}
+
+	};
+}
