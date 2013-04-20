f3411f0cb4ebd54c645df70d7ff7e591374fbe58
nekko1119 <taku5050@mail.goo.ne.jp>
2013-04-20 16:20:37 +0900

    add is_pointer.hpp

---
+++ nek/type_traits/is_pointer.hpp
@@ -0,0 +1,31 @@
+﻿#ifndef NEK_TYPE_TRAITS_IS_POINTER_HPP
+#define NEK_TYPE_TRAITS_IS_POINTER_HPP
+
+#include <nek/type_traits/integral_constant.hpp>
+#include <nek/type_traits/remove_cv.hpp>
+
+namespace nek
+{
+	namespace detail
+	{
+		template <class T>
+		struct is_pointer
+			: public false_type
+		{
+		};
+
+		template <class T>
+		struct is_pointer<T*>
+			: public true_type
+		{
+		};
+	}
+
+	template <class T>
+	struct is_pointer
+		: public detail::is_pointer<typename remove_cv<T>::type>
+	{
+	};
+}
+
+#endif
