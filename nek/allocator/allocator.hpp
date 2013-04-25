c13c7d62f75d131b8e0d0ee6311332ba4bf2c9bb
nekko1119 <taku5050@mail.goo.ne.jp>
2013-04-25 18:11:36 +0900

    add functions

--- nek/allocator/allocator.hpp
+++ nek/allocator/allocator.hpp
@@ -2,8 +2,10 @@
 #define NEK_ALLOCATOR_ALLOCATOR_HPP
 
 #include <cstddef>
+#include <exception>
 #include <nek/type_traits/remove_cv.hpp>
 #include <nek/utility/addressof.hpp>
+#include <nek/utility/forward.hpp>
 
 namespace nek
 {
@@ -44,12 +46,12 @@ namespace nek
 			return *this;
 		}
 
-		pointer* allocate(size_type count, const void* = nullptr) const
+		pointer allocate(size_type count, const void* = nullptr) const
 		{
 			void* ptr = nullptr;
-			if(0U < count && count < max_size())
+			if(count < 1U || max_size() < count || (ptr = ::operator new(count * sizeof(value_type))) == nullptr)
 			{
-				ptr = ::operator new(count * sizeof(value_type));
+				throw std::bad_alloc();
 			}
 			return static_cast<pointer>(ptr);
 		}
@@ -59,12 +61,24 @@ namespace nek
 			::operator delete(ptr);
 		}
 
-		inline pointer address(reference value) const
+		template <class U, class... Args>
+		void construct(U* other, Args... args) const
+		{
+			new (reinterpret_cast<void*>(other)) U(forward<Args>(args)...);
+		}
+
+		template <class U>
+		void destroy(U* other) const
+		{
+			other->~U();
+		}
+
+		inline pointer address(reference value)
 		{
 			return addressof(value);
 		}
 
-		inline const_pointer addressof(const_reference value) const
+		inline const_pointer address(const_reference value) const
 		{
 			return addressof(value);
 		}
@@ -74,6 +88,56 @@ namespace nek
 			return static_cast<size_type>(-1) / sizeof(value_type);
 		}
 	};
+
+	template <>
+	class allocator<void>
+	{
+	public:
+		typedef void value_type;
+		typedef value_type* pointer;
+		typedef value_type* void_pointer;
+		typedef const pointer const_pointer;
+		typedef const void_pointer const_void_pointer;
+		typedef std::size_t size_type;
+		typedef std::ptrdiff_t difference_type;
+
+		template <class U>
+		struct rebind
+		{
+			typedef allocator<U> other;
+		};
+
+		allocator()
+		{
+		}
+
+		allocator(const allocator<void>&)
+		{
+		}
+
+		template <class U>
+		allocator(const allocator<U>&)
+		{
+		}
+
+		template <class U>
+		allocator<void>& operator=(const allocator<U>&)
+		{
+			return *this;
+		}
+	};
+
+	template <class T, class U>
+	inline bool operator==(const allocator<T>&, const allocator<U>&)
+	{
+		return true;
+	}
+
+	template <class T, class U>
+	inline bool operator!=(const allocator<T>& left, const allocator<U>& right)
+	{
+		return !(left == right);
+	}
 }
 
 #endif
---
+++ nektest/allocator_test.cpp
@@ -0,0 +1,73 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include <nek/allocator/allocator.hpp>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace nektest
+{
+	TEST_CLASS(allocator_test)
+	{
+	public:
+		
+		class allocator_test_class
+		{
+		public:
+			allocator_test_class(int i, double d)
+				: a(i), b(d)
+			{
+				Logger::WriteMessage(L"constructor\n");
+			}
+
+			allocator_test_class& operator=(const allocator_test_class& right)
+			{
+				Logger::WriteMessage(L"operator=\n");
+				a = right.a;
+				b = right.b;
+				return *this;
+			}
+
+			~allocator_test_class()
+			{
+				Logger::WriteMessage(L"destructor\n");
+			}
+
+			int a;
+			double b;
+		};
+
+		template <class T>
+		struct type_holder
+		{
+			typedef T type;
+		};
+
+		TEST_METHOD(default_allocator_test)
+		{
+			using namespace nek;
+			allocator<allocator_test_class> alloc_test;
+			allocator_test_class* arr = alloc_test.allocate(10);
+			Assert::ExpectException<std::bad_alloc>([&alloc_test]()
+			{
+				alloc_test.allocate(alloc_test.max_size() + 1);
+			}, L"throw std::bad_alloc");
+			Assert::ExpectException<std::bad_alloc>([&alloc_test]()
+			{
+				alloc_test.allocate(0);
+			}, L"throw std::bad_alloc");
+			arr[9] = allocator_test_class(1, 1.0);
+			Assert::AreEqual(1, arr[9].a, L"arr[9].a == 1");
+			alloc_test.construct(&arr[3], 2, 2.0);
+			Assert::AreEqual(2.0, arr[3].b, L"arr[3].b == 2.0");
+			int a;
+			type_holder<decltype(alloc_test)>::type::template rebind<int>::other alloc_int;
+			Assert::AreEqual(&a, alloc_int.address(a), L"&a == address(a)");
+			Assert::AreEqual(static_cast<std::size_t>(-1) / sizeof(allocator_test_class), alloc_test.max_size(), L"max_size");
+			alloc_test.destroy(arr);
+			Logger::WriteMessage(L"destroyed\n");
+			alloc_test.deallocate(arr, 0U);
+			Logger::WriteMessage(L"deallocated\n");
+			Assert::IsTrue(alloc_test == alloc_int, L"alloc_test == alloc_int");
+		}
+	};
+}
