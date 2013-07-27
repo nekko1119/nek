de8844b0d3414c26b6914cafb460a9b10c8dec39
nekko1119 <taku5050@mail.goo.ne.jp>
2013-07-27 12:13:17 +0900

    add utility/is_xxx.hpp

---
+++ nek/utility/is_xxx.hpp
@@ -0,0 +1,32 @@
+﻿#ifndef NEK_UTILITY_IS_XXX_HPP
+#define NEK_UTILITY_IS_XXX_HPP
+
+#include <nek/type_traits/integral_constant.hpp>
+#include <nek/type_traits/remove_cv.hpp>
+
+#define NEK_IS_XXX(name)\
+namespace nek\
+{\
+    namespace detail\
+    {\
+        template <class T>\
+        struct is_##name\
+            : public false_type\
+        {\
+        };\
+\
+        template <>\
+        struct is_##name<name>\
+            : public true_type\
+        {\
+        };\
+    }\
+\
+    template <class T>\
+    struct is_##name\
+    : public detail::is_##name<typename remove_cv<T>::type>\
+    {\
+    };\
+}
+
+#endif
