a5cd86aeab1ce7b7c4e6eea467c0b544fb0288d3
nekko1119 <taku5050@mail.goo.ne.jp>
2013-07-30 16:48:49 +0900

    add type_traits.hpp

---
+++ nek/type_traits.hpp
@@ -0,0 +1,16 @@
+﻿#ifndef NEK_TYPE_TRAITS_HPP
+#define NEK_TYPE_TRAITS_HPP
+
+#include <nek/type_traits/has_value_type.hpp>
+#include <nek/type_traits/is_class.hpp>
+#include <nek/type_traits/is_floating_point.hpp>
+#include <nek/type_traits/is_integral.hpp>
+#include <nek/type_traits/is_pointer.hpp>
+#include <nek/type_traits/is_reference.hpp>
+#include <nek/type_traits/is_same.hpp>
+#include <nek/type_traits/is_union.hpp>
+#include <nek/type_traits/is_void.hpp>
+#include <nek/type_traits/remove_cv.hpp>
+#include <nek/type_traits/remove_reference.hpp>
+
+#endif
