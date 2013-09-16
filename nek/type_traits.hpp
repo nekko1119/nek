51706cda9bbc3d0452b287b42053fac841efe411
nekko1119 <taku50501119@gmail.com>
2013-09-17 04:47:59 +0900

    add new type traits

--- nek/type_traits.hpp
+++ nek/type_traits.hpp
@@ -1,10 +1,13 @@
 ﻿#ifndef NEK_TYPE_TRAITS_HPP
 #define NEK_TYPE_TRAITS_HPP
 
+#include <nek/type_traits/add_cv.hpp>
 #include <nek/type_traits/enable_if.hpp>
 #include <nek/type_traits/enabler.hpp>
 #include <nek/type_traits/has_value_type.hpp>
 #include <nek/type_traits/is_class.hpp>
+#include <nek/type_traits/is_const.hpp>
+#include <nek/type_traits/is_enum.hpp>
 #include <nek/type_traits/is_floating_point.hpp>
 #include <nek/type_traits/is_integral.hpp>
 #include <nek/type_traits/is_pointer.hpp>
@@ -12,6 +15,9 @@
 #include <nek/type_traits/is_same.hpp>
 #include <nek/type_traits/is_union.hpp>
 #include <nek/type_traits/is_void.hpp>
+#include <nek/type_traits/is_volatile.hpp>
+#include <nek/type_traits/make_unsigned.hpp>
+#include <nek/type_traits/propagate_cv.hpp>
 #include <nek/type_traits/remove_cv.hpp>
 #include <nek/type_traits/remove_reference.hpp>
 
