676653365106bc3e01fa6fbee106a094daf8e619
nekko1119 <taku50501119@gmail.com>
2013-09-17 20:26:31 +0900

    elucidate the intention of "Perfect Forwarding"

--- nek/utility/forward.hpp
+++ nek/utility/forward.hpp
@@ -6,16 +6,13 @@
 namespace nek
 {
     template <class T>
-    inline T&& forward(typename remove_reference<T>::type& value)
+    inline T&& forward(remove_reference_t<T>& value)
     {
         return static_cast<T&&>(value);
     }
 
     template <class T>
-    inline T&& forward(typename remove_reference<T>::type&& value)
-    {
-        return static_cast<T&&>(value);
-    }
+    inline T&& forward(remove_reference_t<T>&& value) = delete;
 }
 
 #endif
