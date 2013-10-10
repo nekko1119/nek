ea0e5940dad5419287237af93468b04a590a5f83
nekko1119 <taku50501119@gmail.com>
2013-10-10 22:18:44 +0900

    rename MACRO name
    rename file name:
    	nek/utility/has_xxx.hpp -> nek/utility/define_has_xxx.hpp
    
    rename macro name:
    	NEK_HAS_XXX_TYPE -> NEK_DEFINE_HAS_XXX_TYPE
    
    use private namespace idiom.

---
+++ nek/utility/define_has_xxx.hpp
@@ -0,0 +1,26 @@
+﻿#ifndef NEK_UTILITY_HAS_XXX_HPP
+#define NEK_UTILITY_HAS_XXX_HPP
+
+#include <nek/type_traits/integral_constant.hpp>
+
+#define NEK_DEFINE_HAS_XXX_TYPE(type)\
+namespace\
+{\
+    namespace type##detail\
+    {\
+        template <class T, class = T::##type##>\
+        true_type has_##type##(int);\
+    \
+        template <class>\
+        false_type has_##type##(long);\
+    }\
+\
+    template <class T>\
+    struct has_##type##\
+        : public decltype(type##detail::has_##type##<T>(0))\
+    {\
+    };\
+}\
+namespace type##detail {}\
+
+#endif
