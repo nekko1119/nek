880c72ebaf017e23bf8df4713cccb0291507e7c7
nekko1119 <taku50501119@gmail.com>
2014-05-06 23:21:01 +0900

    add noncopyable_test.cpp

---
+++ test/utility_test/noncopyable_test.cpp
@@ -0,0 +1,37 @@
+﻿#include <nek/utility/noncopyable.hpp>
+#include <gtest/gtest.h>
+
+namespace
+{
+  struct noncopyable : private nek::noncopyable<noncopyable>
+  {
+    noncopyable() = default;
+
+    // visual C++ 12.0 still doesn't support move constructor and move assign operator.
+    noncopyable(noncopyable&&)
+    {
+    }
+
+    noncopyable& operator=(noncopyable&&)
+    {
+      return *this;
+    }
+  };
+}
+
+TEST(noncopyable_test, copy)
+{
+  // comment out statements should be a complie error.
+  // move is valid
+
+  noncopyable a;
+
+  //noncopyable b = a; // copy
+  //b = a; // copy assign
+
+  noncopyable d = std::move(a); // move
+  noncopyable e;
+  e = std::move(d); // move assign
+
+  SUCCEED();
+}
