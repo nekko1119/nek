c0716629c585e38acd354398cb55674b9bc3c8f0
nekko1119 <taku50501119@gmail.com>
2014-05-07 22:00:12 +0900

    fix forward_test

--- test/utility_test/forward_test.cpp
+++ test/utility_test/forward_test.cpp
@@ -1,24 +1,58 @@
-﻿#include <type_traits>
+﻿#include <string>
+#include <type_traits>
 #include <nek/utility/forward.hpp>
 #include <gtest/gtest.h>
 
+namespace
+{
+  struct forward_logging_mock
+  {
+    std::string log = "";
+    forward_logging_mock() = default;
+    forward_logging_mock(forward_logging_mock const&)
+    {
+      log += "copy";
+    }
+
+    forward_logging_mock(forward_logging_mock&&)
+    {
+      log += "move";
+    }
+
+    forward_logging_mock& operator=(forward_logging_mock const&)
+    {
+      log += "copy op";
+      return *this;
+    }
+
+    forward_logging_mock& operator=(forward_logging_mock&&)
+    {
+      log += "move op";
+      return *this;
+    }
+  };
+}
+
 TEST(forward_test, return_rvalue_reference_if_template_argement_is_value_type)
 {
-  int x = 0;
-  bool b = std::is_same<int&&, decltype(nek::forward<int>(x))>::value;
-  EXPECT_TRUE(b);
+  forward_logging_mock f;
+  decltype(auto) actual = nek::forward<forward_logging_mock>(f);
+  EXPECT_EQ("", actual.log);
+  EXPECT_TRUE(std::is_rvalue_reference<decltype(actual)>::value);
 }
 
 TEST(forward_test, return_rvalue_reference_if_template_argement_is_rvalue_reference_type)
 {
-  int x = 0;
-  bool b = std::is_same<int&&, decltype(nek::forward<int&&>(x))>::value;
-  EXPECT_TRUE(b);
+  forward_logging_mock f;
+  decltype(auto) actual = nek::forward<forward_logging_mock&&>(f);
+  EXPECT_EQ("", actual.log);
+  EXPECT_TRUE(std::is_rvalue_reference<decltype(actual)>::value);
 }
 
 TEST(forward_test, return_lvalue_reference_if_template_argement_is_lvalue_reference_type)
 {
-  int x = 0;
-  bool b = std::is_same<int&, decltype(nek::forward<int&>(x))>::value;
-  EXPECT_TRUE(b);
+  forward_logging_mock f;
+  decltype(auto) actual = nek::forward<forward_logging_mock&>(f);
+  EXPECT_EQ("", actual.log);
+  EXPECT_TRUE(std::is_lvalue_reference<decltype(actual)>::value);
 }
