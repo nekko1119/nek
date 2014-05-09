b753bff540e96947fa01f433f640d90a970c73a5
nekko1119 <taku50501119@gmail.com>
2014-05-10 00:38:17 +0900

    add test case

--- test/utility_test/swap_test.cpp
+++ test/utility_test/swap_test.cpp
@@ -1,21 +1,55 @@
-﻿#include <nek/utility/swap.hpp>
-#include <gtest/gtest.h>
+﻿#include <algorithm>
+#include <iterator>
 #include <string>
+#include <nek/utility/swap.hpp>
+#include <gtest/gtest.h>
 
 TEST(swap_test, primitive)
 {
-  int x = 1;
-  int y = 2;
-  nek::swap(x, y);
-  EXPECT_EQ(2, x);
-  EXPECT_EQ(1, y);
+  // setup
+  int const expect1 = 1;
+  int actual1 = expect1;
+  int const expect2 = 2;
+  int actual2 = expect2;
+
+  // exercise
+  nek::swap(actual1, actual2);
+
+  // verify
+  EXPECT_EQ(expect1, actual2);
+  EXPECT_EQ(expect2, actual1);
 }
 
 TEST(swap_test, std_string)
 {
-  std::string str1 = "abc";
-  std::string str2 = "def";
-  nek::swap(str1, str2);
-  EXPECT_EQ("def", str1);
-  EXPECT_EQ("abc", str2);
+  // setup
+  std::string const expect1 = "abc";
+  std::string actual1 = expect1;
+  std::string const expect2 = "def";
+  std::string actual2 = expect2;
+
+  // exercise
+  nek::swap(actual1, actual2);
+
+  // verify
+  EXPECT_EQ(expect1, actual2);
+  EXPECT_EQ(expect2, actual1);
+}
+
+TEST(swap_test, array_of_int)
+{
+  // setup
+  int const expect1[] = {1, 2, 3};
+  int actual1[3];
+  std::copy(std::begin(expect1), std::end(expect1), std::begin(actual1));
+  int const expect2[] = {10, 20, 30};
+  int actual2[3];
+  std::copy(std::begin(expect2), std::end(expect2), std::begin(actual2));
+
+  // exersice
+  nek::swap(actual1, actual2);
+
+  // verify
+  EXPECT_TRUE(std::equal(std::begin(expect1), std::end(expect1), std::begin(actual2)));
+  EXPECT_TRUE(std::equal(std::begin(expect2), std::end(expect2), std::begin(actual1)));
 }
