935482e031f0629aa68011850ad0784b0ab739a5
nekko1119 <taku50501119@gmail.com>
2014-05-09 23:54:44 +0900

    add test method
     std_string

--- test/utility_test/swap_test.cpp
+++ test/utility_test/swap_test.cpp
@@ -1,5 +1,6 @@
 ﻿#include <nek/utility/swap.hpp>
 #include <gtest/gtest.h>
+#include <string>
 
 TEST(swap_test, primitive)
 {
@@ -8,4 +9,13 @@ TEST(swap_test, primitive)
   nek::swap(x, y);
   EXPECT_EQ(91, x);
   EXPECT_EQ(42, y);
+}
+
+TEST(swap_test, std_string)
+{
+  std::string str1 = "abc";
+  std::string str2 = "def";
+  nek::swap(str1, str2);
+  EXPECT_EQ("def", str1);
+  EXPECT_EQ("abc", str2);
 }
