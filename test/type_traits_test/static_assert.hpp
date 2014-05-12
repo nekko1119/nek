e9ddb36bdd1f059293a04ada093728b813dea8af
nekko1119 <taku50501119@gmail.com>
2014-05-12 20:54:06 +0900

    add static_assert.hpp
     to write static_assert more eaiser.

---
+++ test/type_traits_test/static_assert.hpp
@@ -0,0 +1,8 @@
+﻿#ifndef NEK_TYPE_TRAITS_TEST
+#define NEK_TYPE_TRAITS_TEST
+
+// this macro is in order to use static assert more easier.
+#define STATIC_ASSERT(expression)\
+  static_assert(expression, #expression)
+
+#endif
