7611a9c8e2f7665f1a66d0f80d1bd021ee802eae
nekko1119 <taku50501119@gmail.com>
2014-05-12 23:47:28 +0900

    update static_assert.hpp

--- test/type_traits_test/static_assert.hpp
+++ test/type_traits_test/static_assert.hpp
@@ -1,8 +1,19 @@
 ﻿#ifndef NEK_TYPE_TRAITS_TEST_STATIC_ASSERT_HPP
 #define NEK_TYPE_TRAITS_TEST_STATIC_ASSERT_HPP
 
-// this macro is in order to use static assert more easier.
-#define STATIC_ASSERT(expression)\
-  static_assert(expression, #expression)
+#include <type_traits>
+
+// these macros are in order to use static assert more easier.
+#define STATIC_ASSERT_TRUE(pred)\
+  static_assert(pred::value, #pred "::value == true")
+
+#define STATIC_ASSERT_FALSE(pred)\
+  static_assert(!pred::value, #pred "::value == true")
+
+#define STATIC_ASSERT_EQ(actual_type, expect_type)\
+  static_assert(std::is_same<actual_type, expect_type>::value, "std::is_same<" #actual_type ", " #expect_type ">::value")
+
+#define STATIC_ASSERT_NE(actual_type, expect_type)\
+  static_assert(!std::is_same<actual_type, expect_type>::value, "std::is_same<" #actual_type ", " #expect_type ">::value")
 
 #endif
