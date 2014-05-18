91899de479fa6594d08e1610b68d667f05dd7f7a
nekko1119 <taku50501119@gmail.com>
2014-05-18 22:05:02 +0900

    add is_pointer_test.cpp

---
+++ test/type_traits_test/is_pointer_test.cpp
@@ -0,0 +1,58 @@
+﻿#include <nek/type_traits/is_pointer.hpp>
+#include <gtest/gtest.h>
+#include "static_assert.hpp"
+#include "test_type.hpp"
+
+TEST(is_pointer_test, initialize_true)
+{
+  using namespace type_traits_test;
+  STATIC_ASSERT_TRUE(nek::is_pointer<int*>);
+  STATIC_ASSERT_EQ(nek::is_pointer<int*>::type, nek::true_type);
+  STATIC_ASSERT_EQ(nek::is_pointer<int*>::value_type, bool);
+  EXPECT_EQ(true, nek::is_pointer<int*>());
+  SUCCEED();
+}
+
+TEST(is_pointer_test, initialize_false)
+{
+  STATIC_ASSERT_FALSE(nek::is_pointer<int>);
+  STATIC_ASSERT_EQ(nek::is_pointer<int>::type, nek::false_type);
+  STATIC_ASSERT_EQ(nek::is_pointer<int>::value_type, bool);
+  EXPECT_EQ(false, nek::is_pointer<int>());
+  SUCCEED();
+}
+
+TEST(is_pointer_test, yes)
+{
+  using namespace type_traits_test;
+  STATIC_ASSERT_TRUE(nek::is_pointer<int*>);
+  STATIC_ASSERT_TRUE(nek::is_pointer<int**>);
+  STATIC_ASSERT_TRUE(nek::is_pointer<int const*>);
+  STATIC_ASSERT_TRUE(nek::is_pointer<int volatile*>);
+  STATIC_ASSERT_TRUE(nek::is_pointer<int const volatile*>);
+  STATIC_ASSERT_TRUE(nek::is_pointer<int* const>);
+  STATIC_ASSERT_TRUE(nek::is_pointer<int* volatile>);
+  STATIC_ASSERT_TRUE(nek::is_pointer<int* const volatile>);
+  STATIC_ASSERT_TRUE(nek::is_pointer<f1_type>);
+  STATIC_ASSERT_TRUE(nek::is_pointer<int (*)[]>);
+  SUCCEED();
+}
+
+TEST(is_pointer_test, no)
+{
+  using namespace type_traits_test;
+  STATIC_ASSERT_FALSE(nek::is_pointer<int>);
+  STATIC_ASSERT_FALSE(nek::is_pointer<void>);
+  STATIC_ASSERT_FALSE(nek::is_pointer<std::nullptr_t>);
+  STATIC_ASSERT_FALSE(nek::is_pointer<int&>);
+  STATIC_ASSERT_FALSE(nek::is_pointer<int&&>);
+  STATIC_ASSERT_FALSE(nek::is_pointer<int[]>);
+  STATIC_ASSERT_FALSE(nek::is_pointer<int(&)[]>);
+  STATIC_ASSERT_FALSE(nek::is_pointer<void ()>);
+  STATIC_ASSERT_FALSE(nek::is_pointer<void (&)()>);
+  STATIC_ASSERT_FALSE(nek::is_pointer<int*&>);
+  STATIC_ASSERT_FALSE(nek::is_pointer<int (&)[]>);
+  STATIC_ASSERT_FALSE(nek::is_pointer<mf1_type>);
+  
+  SUCCEED();
+}
