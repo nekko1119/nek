2c2a9a3e032f5a132c375682ccc7b663cb24f78d
nekko1119 <taku50501119@gmail.com>
2014-05-22 00:23:22 +0900

    add has_type_test.cpp

---
+++ test/type_traits_test/has_type_test.cpp
@@ -0,0 +1,33 @@
+﻿#include <nek/type_traits/has_type.hpp>
+#include <gtest/gtest.h>
+#include "static_assert.hpp"
+
+#define NEK_MEMBER_TYPE type
+
+#include "has_member_type.hpp"
+
+#undef NEK_MEMBER_TYPE
+
+TEST(has_type_test, initialize_true)
+{
+  STATIC_ASSERT_TRUE(nek::has_type<has_member_type>);
+  STATIC_ASSERT_EQ(nek::has_type<has_member_type>::type, nek::true_type);
+  STATIC_ASSERT_EQ(nek::has_type<has_member_type>::value_type, bool);
+  EXPECT_EQ(true, nek::has_type<has_member_type>());
+  SUCCEED();
+}
+
+TEST(has_type_test, initialize_false)
+{
+  STATIC_ASSERT_FALSE(nek::has_type<int>);
+  STATIC_ASSERT_EQ(nek::has_type<int>::type, nek::false_type);
+  STATIC_ASSERT_EQ(nek::has_type<int>::value_type, bool);
+  EXPECT_EQ(false, nek::has_type<int>());
+  SUCCEED();
+}
+
+TEST(has__type_test, check)
+{
+  NEK_HAS_MEMBER_TYPE(nek::has_type);
+  SUCCEED();
+}
