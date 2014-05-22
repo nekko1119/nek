c9bc91d22e08704e9d06f61ba40e307f882ac91c
nekko1119 <taku50501119@gmail.com>
2014-05-23 00:54:40 +0900

    add add_const_test.cpp

---
+++ test/type_traits_test/add_const_test.cpp
@@ -0,0 +1,20 @@
+﻿#include <nek/type_traits/add_const.hpp>
+#include <gtest/gtest.h>
+#include "static_assert.hpp"
+
+TEST(add_const_test, check)
+{
+  STATIC_ASSERT_EQ(nek::add_const<int>::type, int const);
+  STATIC_ASSERT_EQ(nek::add_const<int const>::type, int const);
+  STATIC_ASSERT_EQ(nek::add_const<int volatile>::type, int const volatile);
+  STATIC_ASSERT_EQ(nek::add_const<int*>::type, int* const);
+  STATIC_ASSERT_EQ(nek::add_const<int&>::type, int&);
+  STATIC_ASSERT_EQ(nek::add_const<int const&>::type, int const&);
+  STATIC_ASSERT_EQ(nek::add_const<int&&>::type, int&&);
+  STATIC_ASSERT_EQ(nek::add_const<int const&&>::type, int const&&);
+  STATIC_ASSERT_EQ(nek::add_const<int []>::type, int const []);
+  STATIC_ASSERT_EQ(nek::add_const<int const []>::type, int const []);
+  STATIC_ASSERT_EQ(nek::add_const<int (*)[]>::type, int (* const)[]);
+  STATIC_ASSERT_EQ(nek::add_const<int (&)[]>::type, int (&)[]);
+  SUCCEED();
+}
