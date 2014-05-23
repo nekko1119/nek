7f51d6c1b97689f0dad200dd754b9b4927e90b51
nekko1119 <taku50501119@gmail.com>
2014-05-23 22:33:37 +0900

    add enable_if_test.cpp

---
+++ test/type_traits_test/enable_if_test.cpp
@@ -0,0 +1,35 @@
+﻿#include <nek/type_traits/enable_if.hpp>
+#include <gtest/gtest.h>
+#include <nek/type_traits/is_integral.hpp>
+
+template <class T>
+auto func_c(T const&) -> typename nek::enable_if_c<nek::is_integral<T>::value, bool>::type
+{
+  return true;
+}
+
+template <class T>
+auto func_c(T const&) -> typename nek::disable_if_c<nek::is_integral<T>::value, bool>::type
+{
+  return false;
+}
+
+template <class T>
+auto func(T const&) -> typename nek::enable_if<nek::is_integral<T>, bool>::type
+{
+  return true;
+}
+
+template <class T>
+auto func(T const&) -> typename nek::disable_if<nek::is_integral<T>, bool>::type
+{
+  return false;
+}
+
+TEST(enable_if_c_test, check)
+{
+  EXPECT_TRUE(func_c(0));
+  EXPECT_FALSE(func_c(0.0));
+  EXPECT_TRUE(func(0));
+  EXPECT_FALSE(func(0.0));
+}
