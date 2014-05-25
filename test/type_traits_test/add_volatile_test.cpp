d1c8b723da72ed03fe2d2bdbf35e1785d31c34e0
nekko1119 <taku50501119@gmail.com>
2014-05-26 00:18:59 +0900

    add add_volatile_test.cpp

---
+++ test/type_traits_test/add_volatile_test.cpp
@@ -0,0 +1,20 @@
+﻿#include <nek/type_traits/add_volatile.hpp>
+#include <gtest/gtest.h>
+#include "static_assert.hpp"
+
+TEST(add_volatile_test, check)
+{
+  STATIC_ASSERT_EQ(nek::add_volatile<int>::type, int volatile);
+  STATIC_ASSERT_EQ(nek::add_volatile<int volatile>::type, int volatile);
+  STATIC_ASSERT_EQ(nek::add_volatile<int const>::type, int const volatile);
+  STATIC_ASSERT_EQ(nek::add_volatile<int*>::type, int* volatile);
+  STATIC_ASSERT_EQ(nek::add_volatile<int&>::type, int&);
+  STATIC_ASSERT_EQ(nek::add_volatile<int volatile&>::type, int volatile&);
+  STATIC_ASSERT_EQ(nek::add_volatile<int&&>::type, int&&);
+  STATIC_ASSERT_EQ(nek::add_volatile<int volatile&&>::type, int volatile&&);
+  STATIC_ASSERT_EQ(nek::add_volatile<int[]>::type, int volatile[]);
+  STATIC_ASSERT_EQ(nek::add_volatile<int volatile[]>::type, int volatile[]);
+  STATIC_ASSERT_EQ(nek::add_volatile<int (*)[]>::type, int (*volatile)[]);
+  STATIC_ASSERT_EQ(nek::add_volatile<int (&)[]>::type, int(&)[]);
+  SUCCEED();
+}
