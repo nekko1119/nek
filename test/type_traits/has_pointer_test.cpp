7365a702ae083c7e40122b845abd8c0c1675dcbe
nekko1119 <taku50501119@gmail.com>
2014-05-28 02:41:54 +0900

    add has_pointer_test.cpp

---
+++ test/type_traits/has_pointer_test.cpp
@@ -0,0 +1,32 @@
+﻿#include <nek/type_traits/has_pointer.hpp>
+#include <gtest/gtest.h>
+#include "static_assert.hpp"
+
+#define NEK_MEMBER_TYPE pointer
+#include "has_member_type.hpp"
+
+#undef NEK_MEMBER_TYPE
+
+TEST(has_pointer_test, initialize_true)
+{
+  STATIC_ASSERT_TRUE(nek::has_pointer<has_member_type>);
+  STATIC_ASSERT_EQ(nek::has_pointer<has_member_type>::type, nek::true_type);
+  STATIC_ASSERT_EQ(nek::has_pointer<has_member_type>::value_type, bool);
+  EXPECT_EQ(true, nek::has_pointer<has_member_type>());
+  SUCCEED();
+}
+
+TEST(has_pointer_test, initialize_false)
+{
+  STATIC_ASSERT_FALSE(nek::has_pointer<int>);
+  STATIC_ASSERT_EQ(nek::has_pointer<int>::type, nek::false_type);
+  STATIC_ASSERT_EQ(nek::has_pointer<int>::value_type, bool);
+  EXPECT_EQ(false, nek::has_pointer<int>());
+  SUCCEED();
+}
+
+TEST(has_pointer_test, check)
+{
+  NEK_HAS_MEMBER_TYPE(nek::has_pointer);
+  SUCCEED();
+}
