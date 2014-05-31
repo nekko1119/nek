2c8f8e354b2e523aa07d922e3dffbdcaa3823526
nekko1119 <taku50501119@gmail.com>
2014-05-31 19:49:28 +0900

    add add_cv_test.cpp

---
+++ test/type_traits/add_cv_test.cpp
@@ -0,0 +1,23 @@
+﻿#include <nek/type_traits/add_cv.hpp>
+#include <gtest/gtest.h>
+#include "static_assert.hpp"
+
+TEST(add_cv_test, check)
+{
+  STATIC_ASSERT_EQ(nek::add_cv<int>::type, int const volatile);
+  STATIC_ASSERT_EQ(nek::add_cv<int const>::type, int const volatile);
+  STATIC_ASSERT_EQ(nek::add_cv<int volatile>::type, int const volatile);
+  STATIC_ASSERT_EQ(nek::add_cv<int const volatile>::type, int const volatile);
+  STATIC_ASSERT_EQ(nek::add_cv<int*>::type, int* const volatile);
+  STATIC_ASSERT_EQ(nek::add_cv<int volatile*>::type, int volatile* const volatile);
+  STATIC_ASSERT_EQ(nek::add_cv<int const*>::type, int const* const volatile);
+  STATIC_ASSERT_EQ(nek::add_cv<int&>::type, int&);
+  STATIC_ASSERT_EQ(nek::add_cv<int const&>::type, int const&);
+  STATIC_ASSERT_EQ(nek::add_cv<int&&>::type, int&&);
+  STATIC_ASSERT_EQ(nek::add_cv<int const&&>::type, int const&&);
+  STATIC_ASSERT_EQ(nek::add_cv<int []>::type, int const volatile[]);
+  STATIC_ASSERT_EQ(nek::add_cv<int const[]>::type, int const volatile[]);
+  STATIC_ASSERT_EQ(nek::add_cv<int (*)[]>::type, int (*const volatile)[]);
+  STATIC_ASSERT_EQ(nek::add_cv<int (&)[]>::type, int (&)[]);
+  SUCCEED();
+}
