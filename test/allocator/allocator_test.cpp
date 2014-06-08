74f6d8813506cad5ffe98dc7e16b5159982aff8c
nekko1119 <taku50501119@gmail.com>
2014-06-08 18:46:05 +0900

    add allocator_test.cpp

---
+++ test/allocator/allocator_test.cpp
@@ -0,0 +1,16 @@
+﻿#include <nek\allocator\allocator.hpp>
+#include <gtest/gtest.h>
+#include "../type_traits/static_assert.hpp"
+
+TEST(allocator_test, member_type)
+{
+  using type = nek::allocator<int>;
+  STATIC_ASSERT_EQ(type::value_type, int);
+  STATIC_ASSERT_EQ(type::reference, int&);
+  STATIC_ASSERT_EQ(type::const_reference, int const&);
+  STATIC_ASSERT_EQ(type::pointer, int*);
+  STATIC_ASSERT_EQ(type::const_pointer, int const*);
+  STATIC_ASSERT_EQ(type::difference_type, std::ptrdiff_t);
+  STATIC_ASSERT_EQ(type::size_type, std::size_t);
+  STATIC_ASSERT_EQ(type::rebind<double>::other, nek::allocator<double>);
+}
