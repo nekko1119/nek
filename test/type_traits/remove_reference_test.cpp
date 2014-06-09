aa85b55f82171ab3b7ea30fddb9a14395e5bfbc4
nekko1119 <taku50501119@gmail.com>
2014-06-10 01:14:52 +0900

    add remove_reference_test.cpp

---
+++ test/type_traits/remove_reference_test.cpp
@@ -0,0 +1,20 @@
+﻿#include <nek/type_traits/remove_reference.hpp>
+#include <gtest/gtest.h>
+#include "static_assert.hpp"
+
+TEST(remove_reference_test, check)
+{
+  STATIC_ASSERT_EQ(nek::remove_reference<int>::type, int);
+  STATIC_ASSERT_EQ(nek::remove_reference<int const>::type, int const);
+  STATIC_ASSERT_EQ(nek::remove_reference<int volatile>::type, int volatile);
+  STATIC_ASSERT_EQ(nek::remove_reference<int*>::type, int*);
+  STATIC_ASSERT_EQ(nek::remove_reference<int&>::type, int);
+  STATIC_ASSERT_EQ(nek::remove_reference<int const&>::type, int const);
+  STATIC_ASSERT_EQ(nek::remove_reference<int&&>::type, int);
+  STATIC_ASSERT_EQ(nek::remove_reference<int const&&>::type, int const);
+  STATIC_ASSERT_EQ(nek::remove_reference<int []>::type, int []);
+  STATIC_ASSERT_EQ(nek::remove_reference<int (&)[]>::type, int []);
+  STATIC_ASSERT_EQ(nek::remove_reference<int (&)()>::type, int ());
+  STATIC_ASSERT_EQ(nek::remove_reference<int*&>::type, int*);
+  SUCCEED();
+}
