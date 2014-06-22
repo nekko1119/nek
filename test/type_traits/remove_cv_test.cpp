35f4dc45cd3ff083fc9587c2e62ac949db8af6b6
nekko1119 <taku50501119@gmail.com>
2014-06-23 02:50:50 +0900

    add remove_cv_test.cpp

---
+++ test/type_traits/remove_cv_test.cpp
@@ -0,0 +1,23 @@
+﻿#include <nek/type_traits/remove_cv.hpp>
+#include <gtest/gtest.h>
+#include "static_assert.hpp"
+
+TEST(remove_cv_test, check)
+{
+  STATIC_ASSERT_EQ(nek::remove_cv<int>::type, int);
+  STATIC_ASSERT_EQ(nek::remove_cv<int const>::type, int);
+  STATIC_ASSERT_EQ(nek::remove_cv<int volatile>::type, int);
+  STATIC_ASSERT_EQ(nek::remove_cv<int const volatile>::type, int);
+  STATIC_ASSERT_EQ(nek::remove_cv<int const*>::type, int const*);
+  STATIC_ASSERT_EQ(nek::remove_cv<int volatile*>::type, int volatile*);
+  STATIC_ASSERT_EQ(nek::remove_cv<int const volatile*>::type, int const volatile*);
+  STATIC_ASSERT_EQ(nek::remove_cv<int&>::type, int&);
+  STATIC_ASSERT_EQ(nek::remove_cv<int const&>::type, int const&);
+  STATIC_ASSERT_EQ(nek::remove_cv<int&&>::type, int&&);
+  STATIC_ASSERT_EQ(nek::remove_cv<int const&&>::type, int const&&);
+  STATIC_ASSERT_EQ(nek::remove_cv<int []>::type, int []);
+  STATIC_ASSERT_EQ(nek::remove_cv<int const volatile[]>::type, int []);
+  STATIC_ASSERT_EQ(nek::remove_cv<int (*const volatile)[]>::type, int (*)[]);
+  STATIC_ASSERT_EQ(nek::remove_cv<int (&)[]>::type, int (&)[]);
+  SUCCEED();
+}
