5c05e6a12dfda7651652493757d42485c62d3c90
nekko1119 <taku50501119@gmail.com>
2014-07-06 03:33:32 +0900

    add iterator_traits.hpp

---
+++ test/iterator/iterator_traits_test.cpp
@@ -0,0 +1,42 @@
+﻿#include <nek/iterator/iterator_traits.hpp>
+#include <nek/iterator/iterator.hpp>
+#include <gtest/gtest.h>
+#include "../type_traits/static_assert.hpp"
+
+namespace
+{
+  struct test_iterator
+    : public nek::iterator<nek::bidirectional_iterator_tag, int>
+  {
+  };
+}
+
+TEST(iterator_traits_test, iterator)
+{
+  using type = nek::iterator_traits<test_iterator>;
+  STATIC_ASSERT_EQ(type::iterator_category, nek::bidirectional_iterator_tag);
+  STATIC_ASSERT_EQ(type::value_type, int);
+  STATIC_ASSERT_EQ(type::difference_type, std::ptrdiff_t);
+  STATIC_ASSERT_EQ(type::pointer, int*);
+  STATIC_ASSERT_EQ(type::reference, int&);
+}
+
+TEST(iterator_traits_test, pointer)
+{
+  using type = nek::iterator_traits<int*>;
+  STATIC_ASSERT_EQ(type::iterator_category, nek::random_access_iterator_tag);
+  STATIC_ASSERT_EQ(type::value_type, int);
+  STATIC_ASSERT_EQ(type::difference_type, std::ptrdiff_t);
+  STATIC_ASSERT_EQ(type::pointer, int*);
+  STATIC_ASSERT_EQ(type::reference, int&);
+}
+
+TEST(iterator_traits_test, const_pointer)
+{
+  using type = nek::iterator_traits<int const*>;
+  STATIC_ASSERT_EQ(type::iterator_category, nek::random_access_iterator_tag);
+  STATIC_ASSERT_EQ(type::value_type, int);
+  STATIC_ASSERT_EQ(type::difference_type, std::ptrdiff_t);
+  STATIC_ASSERT_EQ(type::pointer, int const*);
+  STATIC_ASSERT_EQ(type::reference, int const&);
+}
--- test/main.cpp
+++ test/main.cpp
@@ -1,15 +1,7 @@
 #include <gtest/gtest.h>
-#include <nek/allocator/allocator.hpp>
-#include <vector>
 
 int main(int argc, char** argv)
 {
-  std::vector<int, nek::allocator<int>> v;
-  v.reserve(10);
-  v.resize(5);
-  v.push_back(1);
-  v.pop_back();
-  v.shrink_to_fit();
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
