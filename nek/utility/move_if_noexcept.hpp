4a801e55504b80785256851ef4e555485732514f
nekko1119 <taku50501119@gmail.com>
2014-07-07 00:22:52 +0900

    add move_if_noexcept.hpp

---
+++ nek/utility/move_if_noexcept.hpp
@@ -0,0 +1,22 @@
+﻿#ifndef NEK_UTILITY_MOVE_IF_NOEXCEPT_HPP
+#define NEK_UTILITY_MOVE_IF_NOEXCEPT_HPP
+
+#include <type_traits> // TODO : is_nothrow_move_constructible, is_copy_constructible
+#include <nek/mpl/if.hpp>
+#include <nek/utility/move.hpp>
+
+namespace nek
+{
+  template <class T>
+  inline auto move_if_noexcept(T& value) noexcept
+    -> nek::mpl::if_c_t<
+    !std::is_nothrow_move_constructible<T>::value &&
+    std::is_copy_constructible<T>::value,
+    T const&,
+    T&&>
+  {
+    return nek::move(value);
+  }
+}
+
+#endif
---
+++ test/utility/move_if_noexcept_test.cpp
@@ -0,0 +1,54 @@
+﻿#include <nek/utility/move_if_noexcept.hpp>
+#include <gtest/gtest.h>
+#include <string>
+
+namespace
+{
+  struct noexceptable
+  {
+    std::string log = "";
+
+    noexceptable() = default;
+
+    noexceptable(noexceptable const&) noexcept
+    {
+      log += "copy ctor";
+    }
+
+      noexceptable(noexceptable&&) noexcept
+    {
+      log += "move ctor";
+    }
+  };
+  
+  struct exceptable
+  {
+    std::string log = "";
+
+    exceptable() = default;
+
+    exceptable(exceptable const&)
+    {
+      log += "copy ctor";
+    }
+
+    exceptable(exceptable&&)
+    {
+      log += "move ctor";
+    }
+  };
+}
+
+TEST(move_if_noexcept_test, except)
+{
+  exceptable e;
+  auto actual = nek::move_if_noexcept(e);
+  EXPECT_EQ("copy ctor", actual.log);
+}
+
+TEST(move_if_noexcept_test, noexcept)
+{
+  noexceptable e;
+  auto actual = nek::move_if_noexcept(e);
+  EXPECT_EQ("move ctor", actual.log);
+}
