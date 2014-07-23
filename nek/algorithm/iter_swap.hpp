84ff72ad0d23057c7c6e74dcff7b02aefe4e93b3
nekko1119 <taku50501119@gmail.com>
2014-07-24 00:34:42 +0900

    add iter_swap.hpp

---
+++ nek/algorithm/iter_swap.hpp
@@ -0,0 +1,15 @@
+#ifndef NEK_ALGORITHM_ITER_SWAP_HPP
+#define NEK_ALGORITHM_ITER_SWAP_HPP
+
+#include <nek/utility/swap.hpp>
+
+namespace nek
+{
+  template <class LeftForwardIterator, class RightForwardIterator>
+  inline void iter_swap(LeftForwardIterator left, RightForwardIterator right) noexcept
+  {
+    swap(*left, *right);
+  }
+}
+
+#endif
