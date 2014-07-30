5f9516b3f2599815a22e428834048a342e30f062
nekko1119 <taku50501119@gmail.com>
2014-07-31 01:06:12 +0900

    add at

--- nek/container/function.hpp
+++ nek/container/function.hpp
@@ -1,6 +1,7 @@
 #ifndef NEK_CONTAINER_FUNCTION_HPP
 #define NEK_CONTAINER_FUNCTION_HPP
 
+#include <nek/container/function/at.hpp>
 #include <nek/container/function/emplace_back.hpp>
 #include <nek/container/function/is_empty.hpp>
 #include <nek/container/function/size.hpp>
---
+++ nek/container/function/at.hpp
@@ -0,0 +1,40 @@
+﻿#ifndef NEK_CONTAINER_FUNCTION_AT_HPP
+#define NEK_CONTAINER_FUNCTION_AT_HPP
+
+#include <stdexcept>
+#include <nek/container/function/size.hpp>
+
+namespace nek
+{
+  template <class Container, class Size>
+  decltype(auto) at(Container& con, Size n)
+  {
+    using size_type = typename Container::size_type;
+    if (nek::size(con) < static_cast<size_type>(n)) {
+      throw std::out_of_range{"nek::at"};
+    }
+    return con[n];
+  }
+
+  template <class Container, class Size>
+  decltype(auto) at(Container const& con, Size n)
+  {
+    using size_type = typename Container::size_type;
+    if (nek::size(con) < static_cast<size_type>(n)) {
+      throw std::out_of_range{"nek::at"};
+    }
+    return con[n];
+  }
+
+  template <class Container, class Size>
+  decltype(auto) cat(Container const& con, Size n)
+  {
+    using size_type = typename Container::size_type;
+    if (nek::size(con) < static_cast<size_type>(n)) {
+      throw std::out_of_range{"nek::at"};
+    }
+    return con[n];
+  }
+}
+
+#endif
