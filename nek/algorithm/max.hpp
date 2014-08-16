f05a24d8ed42465d301f628b10cba674d53971b9
nekko1119 <taku50501119@gmail.com>
2014-08-16 20:26:47 +0900

    add max

---
+++ nek/algorithm/max.hpp
@@ -0,0 +1,13 @@
+﻿#ifndef NEK_ALGORITHM_MAX_HPP
+#define NEK_ALGORITHM_MAX_HPP
+
+namespace nek
+{
+  template <class T>
+  inline T const& max(T const& left, T const& right)
+  {
+    return left < right ? right : left;
+  }
+}
+
+#endif
