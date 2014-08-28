5837378ff15d3789fd0e48d45a9406691e944376
nekko1119 <taku50501119@gmail.com>
2014-08-29 02:25:15 +0900

    add back

---
+++ nek/container/function/back.hpp
@@ -0,0 +1,33 @@
+﻿#ifndef NEK_CONTAINER_FUNCTION_BACK_HPP
+#define NEK_CONTAINER_FUNCTION_BACK_HPP
+
+#include <cstddef>
+
+namespace nek
+{
+  template <class Container>
+  decltype(auto) back(Container& con)
+  {
+    return *--con.end();
+  }
+
+  template <class Container>
+  decltype(auto) back(Container const& con)
+  {
+    return *--con.end();
+  }
+
+  template <class T, std::size_t N>
+  constexpr decltype(auto) back(T(&array)[N])
+  {
+    return array[N - 1];
+  }
+
+  template <class T, std::size_t N>
+  constexpr decltype(auto) back(T const (&array)[N])
+  {
+    return array[N - 1];
+  }
+}
+
+#endif
