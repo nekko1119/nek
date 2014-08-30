335b029efa4b7fa81d28a30cb9fef0beb981c444
nekko1119 <taku50501119@gmail.com>
2014-08-30 13:03:45 +0900

    add back_test.cpp

---
+++ test/container/function/back_test.cpp
@@ -0,0 +1,17 @@
+﻿#include <nek/container/function/back.hpp>
+#include <gtest/gtest.h>
+#include <list>
+
+TEST(back_test, list)
+{
+  std::list<int> const l = {1, 2, 3};
+  EXPECT_EQ(3, nek::back(l));
+}
+
+TEST(back_test, array)
+{
+  constexpr int a[] = {1, 2, 3};
+  // CTP 2013's constexpr is very poor.
+  //static_assert(1 == nek::front(a), "");
+  EXPECT_EQ(3, nek::back(a));
+}
