832221bfc7edbd6bcd908b67647818a47b358d13
nekko1119 <taku50501119@gmail.com>
2014-09-19 00:31:14 +0900

    add remove_all_extends

---
+++ nek/type_traits/remove_all_extends.hpp
@@ -0,0 +1,27 @@
+﻿#ifndef NEK_TYPE_TRAITS_REMOVE_ALL_EXTENDS_HPP
+#define NEK_TYPE_TRAITS_REMOVE_ALL_EXTENDS_HPP
+
+#include <type_traits>
+
+namespace nek
+{
+  template <class T>
+  struct remove_all_extends
+  {
+    using type = T;
+  };
+
+  template <class T, std::size_t N>
+  struct remove_all_extends<T[N]>
+  {
+    using type = typename remove_all_extends<T>::type;
+  };
+
+  template <class T>
+  struct remove_all_extends<T[]>
+  {
+    using type = typename remove_all_extends<T>::type;
+  };
+}
+
+#endif
---
+++ test/type_traits/remove_all_extends.cpp
@@ -0,0 +1,23 @@
+#include <nek/type_traits/remove_all_extends.hpp>
+#include <gtest/gtest.h>
+#include "static_assert.hpp"
+
+TEST(remove_all_extends_test, normal)
+{
+  STATIC_ASSERT_EQ(nek::remove_all_extends<int>::type, int);
+  STATIC_ASSERT_EQ(nek::remove_all_extends<int[]>::type, int);
+  STATIC_ASSERT_EQ(nek::remove_all_extends<int[1]>::type, int);
+  STATIC_ASSERT_EQ(nek::remove_all_extends<int[][1]>::type, int);
+  STATIC_ASSERT_EQ(nek::remove_all_extends<int[1][1]>::type, int);
+  STATIC_ASSERT_EQ(nek::remove_all_extends<int const>::type, int const);
+  STATIC_ASSERT_EQ(nek::remove_all_extends<int volatile>::type, int volatile);
+  STATIC_ASSERT_EQ(nek::remove_all_extends<int const volatile>::type, int const volatile);
+  STATIC_ASSERT_EQ(nek::remove_all_extends<int const[]>::type, int const);
+  STATIC_ASSERT_EQ(nek::remove_all_extends<int volatile[]>::type, int volatile);
+  STATIC_ASSERT_EQ(nek::remove_all_extends<int const volatile[]>::type, int const volatile);
+  STATIC_ASSERT_EQ(nek::remove_all_extends<int (*)[]>::type, int (*)[]);
+  STATIC_ASSERT_EQ(nek::remove_all_extends<int (&)[]>::type, int (&)[]);
+  STATIC_ASSERT_EQ(nek::remove_all_extends<int (*[])()>::type, int (*)());
+  STATIC_ASSERT_EQ(nek::remove_all_extends<int*[]>::type, int*);
+  STATIC_ASSERT_EQ(nek::remove_all_extends<int const* const[]>::type, int const* const);
+}
