bb416d0ba07dfcc675cc840aca9e40efc5628268
nekko1119 <taku50501119@gmail.com>
2014-09-20 21:25:44 +0900

    add is_array

---
+++ nek/type_traits/is_array.hpp
@@ -0,0 +1,28 @@
+﻿#ifndef NEK_TYPE_TRAITS_IS_ARRAY_HPP
+#define NEK_TYPE_TRAITS_IS_ARRAY_HPP
+
+#include <cstddef>
+#include <nek/type_traits/integral_constant.hpp>
+
+namespace nek
+{
+  template <class T>
+  struct is_array
+    : false_type
+  {
+  };
+
+  template <class T>
+  struct is_array<T[]>
+    : true_type
+  {
+  };
+
+  template <class T, std::size_t N>
+  struct is_array<T[N]>
+    : true_type
+  {
+  };
+}
+
+#endif
---
+++ test/type_traits/is_array_test.cpp
@@ -0,0 +1,41 @@
+#include <nek/type_traits/is_array.hpp>
+#include <gtest/gtest.h>
+#include <array>
+#include "static_assert.hpp"
+
+TEST(is_array_test, initialize_true)
+{
+  STATIC_ASSERT_TRUE(nek::is_array<int[]>);
+  STATIC_ASSERT_EQ(nek::is_array<int[]>::type, nek::true_type);
+  STATIC_ASSERT_EQ(nek::is_array<int[]>::value_type, bool);
+  EXPECT_EQ(true, nek::is_array<int[]>());
+}
+
+TEST(is_array_test, initialize_false)
+{
+  STATIC_ASSERT_FALSE(nek::is_array<int>);
+  STATIC_ASSERT_EQ(nek::is_array<int>::type, nek::false_type);
+  STATIC_ASSERT_EQ(nek::is_array<int>::value_type, bool);
+  EXPECT_EQ(false, nek::is_array<int>());
+}
+
+TEST(is_array_test, yes)
+{
+  STATIC_ASSERT_TRUE(nek::is_array<int[]>);
+  STATIC_ASSERT_TRUE(nek::is_array<int[1]>);
+  STATIC_ASSERT_TRUE(nek::is_array<int[][1]>);
+  STATIC_ASSERT_TRUE(nek::is_array<int[1][1]>);
+  STATIC_ASSERT_TRUE(nek::is_array<int const volatile []>);
+  STATIC_ASSERT_TRUE(nek::is_array<int* []>);
+  STATIC_ASSERT_TRUE(nek::is_array<void (*[])()>);
+}
+
+TEST(is_array_test, no)
+{
+  STATIC_ASSERT_FALSE(nek::is_array<int (*)[]>);
+  STATIC_ASSERT_FALSE(nek::is_array<int (&)[]>);
+  STATIC_ASSERT_FALSE(nek::is_array<int*>);
+  using array_type = std::array<int, 1>;
+  STATIC_ASSERT_FALSE(nek::is_array<array_type>);
+  STATIC_ASSERT_FALSE(nek::is_array<void (int (&)[1])>);
+}
