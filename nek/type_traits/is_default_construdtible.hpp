3e42c6e33be5ccc50886b8b3654b8fe3703e7f79
nekko1119 <taku50501119@gmail.com>
2014-09-21 23:59:00 +0900

    add is_default_constructible

---
+++ nek/type_traits/is_default_construdtible.hpp
@@ -0,0 +1,52 @@
+﻿#ifndef NEK_TYPE_TRAITS_IS_DEFAULT_CONSTRUCTIBLE_HPP
+#define NEK_TYPE_TRAITS_IS_DEFAULT_CONSTRUCTIBLE_HPP
+
+#include <nek/mpl/and.hpp>
+#include <nek/mpl/not.hpp>
+#include <nek/mpl/or.hpp>
+#include <nek/type_traits/is_array.hpp>
+#include <nek/type_traits/is_void.hpp>
+#include <nek/type_traits/remove_all_extends.hpp>
+
+namespace nek
+{
+  namespace is_default_constructible_detail
+  {
+    template <class T>
+    struct is_default_constructible_
+    {
+    private:
+      template <class U, class = decltype(U())>
+      static nek::true_type check(int);
+
+      template <class U>
+      static nek::false_type check(long);
+
+    public:
+      using type = decltype(check<T>(0));
+    };
+
+    template <class T>
+    struct is_default_constructible_not_void
+      : public nek::integral_constant<bool, mpl::and_<mpl::not_<is_void<T>>, is_default_constructible_<T>>::value>
+    {
+    };
+
+    template <class T, class = nek::is_array<T>>
+    struct is_default_constructible_is_array;
+
+    template <class T>
+    struct is_default_constructible_is_array<T, nek::false_type>
+      : public is_default_constructible_not_void<T>
+    {
+    };
+  }
+
+  template <class T>
+  struct is_default_constructible
+    : public is_default_constructible_detail::is_default_constructible_is_array<T>
+  {
+  };
+}
+
+#endif
---
+++ test/type_traits/is_default_constructible_test.cpp
@@ -0,0 +1,12 @@
+﻿#include <nek/type_traits/is_default_construdtible.hpp>
+#include <gtest/gtest.h>
+#include "static_assert.hpp"
+
+TEST(is_default_constructible_test, initialize_true)
+{
+  using type = nek::is_default_constructible<int>;
+  STATIC_ASSERT_TRUE(type);
+  STATIC_ASSERT_EQ(type, nek::true_type);
+  STATIC_ASSERT_EQ(type::value_type, bool);
+  EXPECT_EQ(true, type());
+}
