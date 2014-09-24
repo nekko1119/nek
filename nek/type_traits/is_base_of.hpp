ed299b589611fadaa532edba3a91279f1d9eecf9
nekko1119 <taku50501119@gmail.com>
2014-09-24 23:51:14 +0900

    add is_base_of

---
+++ nek/type_traits/is_base_of.hpp
@@ -0,0 +1,54 @@
+﻿#ifndef NEK_TYPE_TRAITS_IS_BASE_OF_HPP
+#define NEK_TYPE_TRAITS_IS_BASE_OF_HPP
+
+#include <nek/mpl/and.hpp>
+#include <nek/mpl/not.hpp>
+#include <nek/type_traits/integral_constant.hpp>
+#include <nek/type_traits/is_class.hpp>
+#include <nek/type_traits/is_same.hpp>
+#include <nek/type_traits/remove_cv.hpp>
+
+namespace nek
+{
+  namespace is_base_of_detail
+  {
+    template <class Base, class Derived, bool = mpl::and_c<nek::is_class<Base>::value, nek::is_class<Derived>::value, mpl::not_c<nek::is_same<Base, Derived>::value>::value>::value>
+    struct is_base_of_
+    {
+    private:
+      static nek::true_type check(Derived&, int);
+      static nek::false_type check(Base&, long);
+
+      struct host
+      {
+        operator Base&();
+        operator Derived&();
+      };
+
+    public:
+      using type = decltype(check(host{}, 0));
+    };
+
+    template <class Base, class Derived>
+    struct is_base_of_<Base, Derived, false>
+    {
+      using type = nek::false_type;
+    };
+
+    template <class Base, class Derived>
+    struct is_base_of_is_class
+    {
+      static constexpr bool value =
+        is_base_of_<Base, Derived>::type::value ||
+        nek::is_class<Base>::value && nek::is_same<Base, Derived>::value;
+    };
+  }
+
+  template <class Base, class Derived>
+  struct is_base_of
+    : public nek::integral_constant<bool, is_base_of_detail::is_base_of_is_class<Base, Derived>::value>
+  {
+  };
+}
+
+#endif
---
+++ test/type_traits/is_base_of_test.cpp
@@ -0,0 +1,44 @@
+﻿#include <nek/type_traits/is_base_of.hpp>
+#include <gtest/gtest.h>
+#include "static_assert.hpp"
+
+namespace
+{
+  struct b
+  {
+  };
+
+  struct d
+    : private b
+  {
+  };
+
+  struct other
+  {};
+}
+
+TEST(is_base_of_test, initialize_true)
+{
+}
+
+TEST(is_base_of_test, yes)
+{
+  using bd_type = nek::is_base_of<b, d>;
+  using bb_type = nek::is_base_of<b, b>;
+  using cvbd_type = nek::is_base_of<b const volatile, d>;
+  STATIC_ASSERT_TRUE(bd_type);
+  STATIC_ASSERT_TRUE(bb_type);
+  STATIC_ASSERT_TRUE(cvbd_type);
+}
+
+TEST(is_base_of_test, no)
+{
+  using int_int_type = nek::is_base_of<int, int>;
+  using int_char_type = nek::is_base_of<int, char>;
+  using b_other_type = nek::is_base_of<b, other>;
+  using other_other_type = nek::is_base_of<other, other>;
+  STATIC_ASSERT_FALSE(int_int_type);
+  STATIC_ASSERT_FALSE(int_char_type);
+  STATIC_ASSERT_FALSE(b_other_type);
+  STATIC_ASSERT_FALSE(other_other_type);
+}
