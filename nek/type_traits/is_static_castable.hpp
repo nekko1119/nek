a6114898abaa7476de7cea389e5736382e3f577c
nekko1119 <taku50501119@gmail.com>
2014-09-27 20:13:10 +0900

    add is_static_castable

---
+++ nek/type_traits/is_static_castable.hpp
@@ -0,0 +1,33 @@
+﻿#ifndef NEK_TYPE_TRAITS_IS_STATIC_CASTABLE_HPP
+#define NEK_TYPE_TRAITS_IS_STATIC_cASTABLE_HPP
+
+#include <nek/type_traits/integral_constant.hpp>
+#include <nek/utility/declval.hpp>
+
+namespace nek
+{
+  namespace is_static_castable_detail
+  {
+    template <class From, class To>
+    struct is_static_castable_
+    {
+    private:
+      template <class From2, class To2, class = decltype(static_cast<To>(nek::declval<From>()))>
+      static nek::true_type check(int);
+
+      template <class, class>
+      static nek::false_type check(long);
+
+    public:
+      using type = decltype(check<From, To>(0));
+    };
+  }
+
+  template <class From, class To>
+  struct is_static_castable
+    : is_static_castable_detail::is_static_castable_<From, To>::type
+  {
+  };
+}
+
+#endif
