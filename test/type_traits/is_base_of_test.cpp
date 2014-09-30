33a36bf176620c047422718c47e2a781e3d15768
nekko1119 <taku50501119@gmail.com>
2014-10-01 00:35:01 +0900

    add tests

--- test/type_traits/is_base_of_test.cpp
+++ test/type_traits/is_base_of_test.cpp
@@ -6,6 +6,7 @@ namespace
 {
   struct b
   {
+    virtual ~b() {}
   };
 
   struct d
@@ -19,6 +20,20 @@ namespace
 
 TEST(is_base_of_test, initialize_true)
 {
+  using actual_type = nek::is_base_of<b, d>;
+  STATIC_ASSERT_TRUE(actual_type);
+  STATIC_ASSERT_EQ(actual_type::type, nek::true_type);
+  STATIC_ASSERT_EQ(actual_type::value_type, bool);
+  EXPECT_EQ(true, actual_type());
+}
+
+TEST(is_base_of_test, initialize_false)
+{
+  using actual_type = nek::is_base_of<int, int>;
+  STATIC_ASSERT_FALSE(actual_type);
+  STATIC_ASSERT_EQ(actual_type::type, nek::false_type);
+  STATIC_ASSERT_EQ(actual_type::value_type, bool);
+  EXPECT_EQ(false, actual_type());
 }
 
 TEST(is_base_of_test, yes)
@@ -38,7 +53,13 @@ TEST(is_base_of_test, no)
   using int_int_type = nek::is_base_of<int, int>;
   using int_char_type = nek::is_base_of<int, char>;
   using b_other_type = nek::is_base_of<b, other>;
+  using refb_d_type = nek::is_base_of<b&, d>;
+  using rbrd_type = nek::is_base_of<b&, d&>;
+  using pbpd_type = nek::is_base_of<b*, d*>;
   STATIC_ASSERT_FALSE(int_int_type);
   STATIC_ASSERT_FALSE(int_char_type);
   STATIC_ASSERT_FALSE(b_other_type);
+  STATIC_ASSERT_FALSE(refb_d_type);
+  STATIC_ASSERT_FALSE(rbrd_type);
+  STATIC_ASSERT_FALSE(pbpd_type);
 }
