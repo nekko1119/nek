c10b1be3667223afc64952bac848a733b3b513b9
nekko1119 <taku50501119@gmail.com>
2014-10-18 01:01:17 +0900

    fix coding style

--- test/type_traits/enable_if_test.cpp
+++ test/type_traits/enable_if_test.cpp
@@ -5,31 +5,31 @@
 template <class T>
 auto func_c(T const&) -> typename nek::enable_if_c<nek::is_integral<T>::value, bool>::type
 {
-  return true;
+	return true;
 }
 
 template <class T>
 auto func_c(T const&) -> typename nek::disable_if_c<nek::is_integral<T>::value, bool>::type
 {
-  return false;
+	return false;
 }
 
 template <class T>
 auto func(T const&) -> typename nek::enable_if<nek::is_integral<T>, bool>::type
 {
-  return true;
+	return true;
 }
 
 template <class T>
 auto func(T const&) -> typename nek::disable_if<nek::is_integral<T>, bool>::type
 {
-  return false;
+	return false;
 }
 
 TEST(enable_if_c_test, normal)
 {
-  EXPECT_TRUE(func_c(0));
-  EXPECT_FALSE(func_c(0.0));
-  EXPECT_TRUE(func(0));
-  EXPECT_FALSE(func(0.0));
+	EXPECT_TRUE(func_c(0));
+	EXPECT_FALSE(func_c(0.0));
+	EXPECT_TRUE(func(0));
+	EXPECT_FALSE(func(0.0));
 }
--- test/type_traits/test_type.hpp
+++ test/type_traits/test_type.hpp
@@ -3,89 +3,89 @@
 
 namespace type_traits_test
 {
-  struct empty_type
-  {
-    empty_type() = default;
-    ~empty_type() = default;
-    empty_type(empty_type const&) = default;
-    empty_type& operator=(empty_type const&) = default;
-  };
+	struct empty_type
+	{
+		empty_type() = default;
+		~empty_type() = default;
+		empty_type(empty_type const&) = default;
+		empty_type& operator=(empty_type const&) = default;
+	};
 
-  struct class_type
-  {
-    int x;
+	struct class_type
+	{
+		int x;
 
-    class_type() = default;
-    ~class_type() = default;
-    class_type(class_type const&) = default;
-    class_type& operator=(class_type const&) = default;
+		class_type() = default;
+		~class_type() = default;
+		class_type(class_type const&) = default;
+		class_type& operator=(class_type const&) = default;
 
-    void f1();
-    int f2();
-    int f3(int);
-    int f4(int) const;
-  };
+		void f1();
+		int f2();
+		int f3(int);
+		int f4(int) const;
+	};
 
-  enum enum_type
-  {
-    first,
-    second,
-    third
-  };
+	enum enum_type
+	{
+		first,
+		second,
+		third
+	};
 
-  union union_type
-  {
-    ~union_type() = default;
-    int x;
-    double y;
-  };
+	union union_type
+	{
+		~union_type() = default;
+		int x;
+		double y;
+	};
 
-  struct standard_layout_type
-    : public class_type
-  {
-    standard_layout_type(){}
-    ~standard_layout_type(){}
-    standard_layout_type(standard_layout_type const&){}
-    standard_layout_type& operator=(standard_layout_type const&){ return *this; }
-  };
+	struct standard_layout_type
+		: public class_type
+	{
+		standard_layout_type(){}
+		~standard_layout_type(){}
+		standard_layout_type(standard_layout_type const&){}
+		standard_layout_type& operator=(standard_layout_type const&){ return *this; }
+	};
 
-  struct trivially_copyable_type
-    : public class_type
-  {
-    double y;
+	struct trivially_copyable_type
+		: public class_type
+	{
+		double y;
 
-    trivially_copyable_type(){}
-    ~trivially_copyable_type() = default;
-    trivially_copyable_type(trivially_copyable_type const&) = default;
-    trivially_copyable_type& operator=(trivially_copyable_type const&) = default;
-  };
+		trivially_copyable_type(){}
+		~trivially_copyable_type() = default;
+		trivially_copyable_type(trivially_copyable_type const&) = default;
+		trivially_copyable_type& operator=(trivially_copyable_type const&) = default;
+	};
 
-  struct pod_type
-    : public class_type
-  {
-    pod_type() = default;
-    ~pod_type() = default;
-    pod_type(pod_type const&) = default;
-    pod_type& operator=(pod_type const&) = default;
-  };
+	struct pod_type
+		: public class_type
+	{
+		pod_type() = default;
+		~pod_type() = default;
+		pod_type(pod_type const&) = default;
+		pod_type& operator=(pod_type const&) = default;
+	};
 
-  struct nothrow_type
-  {
-    nothrow_type() noexcept {}
-    ~nothrow_type() noexcept {}
-    nothrow_type(nothrow_type const&) noexcept {}
-    nothrow_type& operator=(nothrow_type const&) noexcept { return *this; }
-    nothrow_type(nothrow_type&&) noexcept {}
-    nothrow_type& operator=(nothrow_type&&) noexcept { return *this; }
-  };
+	struct nothrow_type
+	{
+		nothrow_type() noexcept{}
+			~nothrow_type() noexcept{}
+			nothrow_type(nothrow_type const&) noexcept{}
+		nothrow_type& operator=(nothrow_type const&)noexcept{return *this;}
+			nothrow_type(nothrow_type&&) noexcept{}
+		nothrow_type& operator=(nothrow_type&&)noexcept{return *this;}
+	};
 
-  using f1_type = void (*)();
-  using f2_type = int (*)();
-  using f3_type = int (*)(int);
-  using mf1_type = void (class_type::*)();
-  using mf2_type = int (class_type::*)();
-  using mf3_type = int (class_type::*)(int);
-  using mf4_type = int (class_type::*)(int) const;
+	using f1_type = void(*)();
+	using f2_type = int(*)();
+	using f3_type = int(*)(int);
+	using mf1_type = void (class_type::*)();
+	using mf2_type = int (class_type::*)();
+	using mf3_type = int (class_type::*)(int);
+	using mf4_type = int (class_type::*)(int) const;
 }
 
 #endif
