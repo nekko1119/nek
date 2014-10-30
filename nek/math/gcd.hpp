2b31a5c7e9f5b2dad09314fcb5d1ad1b2ff8fbbe
nekko1119 <taku50501119@gmail.com>
2014-10-31 01:30:14 +0900

    add gcd

---
+++ nek/math/gcd.hpp
@@ -0,0 +1,23 @@
+﻿#ifndef NEK_MATM_GCD_HPP
+#define NEK_MATH_GCD_HPP
+
+#include <boost/math/common_factor_rt.hpp>
+
+namespace nek
+{
+	namespace math
+	{
+		template <class T>
+		T gcd(T a, T b)
+		{
+			while (b != static_cast<T>(0)) {
+				auto const r = a % b;
+				a = b;
+				b = r;
+			}
+			return a;
+		}
+	}
+}
+
+#endif
