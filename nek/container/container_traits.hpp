6271cf2b5290de2b907b66c4679dd494f40df800
nekko1119 <taku50501119@gmail.com>
2014-12-09 01:30:41 +0900

    add container utilities

---
+++ nek/container/container_traits.hpp
@@ -0,0 +1,23 @@
+﻿#ifndef NEK_CONTAINER_CONTAINER_TRAITS_HPP
+#define NEK_CONTAINER_CONTAINER_TRAITS_HPP
+
+#include <cstddef>
+
+namespace nek
+{
+    template <class Container>
+    struct container_traits
+    {
+        using value_type = typename Container::value_type;
+        using size_type = typename Container::size_type;
+    };
+
+    template <class T, std::size_t N>
+    struct container_traits<T [N]>
+    {
+        using value_type = T;
+        using size_type = std::size_t;
+    };
+}
+
+#endif
---
+++ nek/container/function/data.hpp
@@ -0,0 +1,31 @@
+﻿#ifndef NEK_CONTAINER_FUNCTION_DATA_HPP
+#define NEK_CONTAINER_FUNCTION_DATA_HPP
+
+namespace nek
+{
+    template <class Container>
+    inline auto data(Container& con)
+    {
+        return con.data();
+    }
+
+    template <class Container>
+    inline auto data(Container const& con)
+    {
+        return con.data();
+    }
+
+    template <class T, std::size_t N>
+    inline constexpr T* data(T (&ar[N]))
+    {
+        return ar;
+    }
+
+    template <class T, std::size_t N>
+    inline constexpr T const* data(T const (&ar[N]))
+    {
+        return ar;
+    }
+}
+
+#endif
