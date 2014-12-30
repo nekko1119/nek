b3b59d80b1cb289df7c85d706bc57e692afe975e
nekko1119 <taku50501119@gmail.com>
2014-12-31 00:28:09 +0900

    fix cmake files

--- test/files.cmake
+++ test/files.cmake
@@ -13,6 +13,7 @@ SET(TEST_SRCS ./algorithm/copy_backward_test.cpp
               ./allocator/allocator_test.cpp
               ./allocator/allocator_traits_test.cpp
               ./any/any_test.cpp
+              ./container/container_traits/container_traits_test.cpp
               ./container/function/at_test.cpp
               ./container/function/back_test.cpp
               ./container/function/clear_test.cpp
